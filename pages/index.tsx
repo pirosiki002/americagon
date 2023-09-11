1971ea4df8dce7bf25fec1d5b939d456f76501a0
SI <>
2023-09-11 14:17:10 +0900

    ✨ message translate JP to EN

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Dictionary = () => {
     <div>
       <input
         type="text"
-        placeholder="英語の単語を入力"
+        placeholder="Please English words"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
