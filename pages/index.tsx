12ee4d03f187a6897cb1b17784da326f51a88e2c
SI <>
2023-09-11 14:05:38 +0900

    new project start

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const Dictionary = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState<string[]>([]);
+
+  const handleSearch = async () => {
+    const response = await fetch('/kantan-ej-dictionary.json');
+    const data = await response.json();
+
+    if (data[query]) {
+      setResults(data[query].ja);
+    } else {
+      setResults([]);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="英語の単語を入力"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>検索</button>
+      <ul>
+        {results.map((result, index) => (
+          <li key={index}>{result}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Dictionary;
