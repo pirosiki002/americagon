1fb627b9cee66097547225801aba16436fbed86e
SI <>
2024-05-11 10:19:24 +0900

    feat: 文字の判定処理（空）を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,20 @@ import InputTable from "src/components/InputTable";
 import ResetButton from "../components/ResetButton";
 import styles from "./index.module.css";
 
+// 盤面の初期化
 const createInitialBoard = (size: number) => {
   return Array(size)
     .fill("")
     .map(() => Array(size).fill(""));
 };
+
+// TODO:英単語が成立しているかをチェック
+function useWordCheck(board: string[][]): boolean {
+  // Perform the word check here and return the result
+
+  return true;
+}
+
 
 // テーブルを表示している大元のコンポーネント
 export default function Home() {
@@ -21,11 +30,15 @@ export default function Home() {
     setBoard(createInitialBoard(size));
   };
 
+  // 単語が有効かどうかをチェック
+  const isWordValid = useWordCheck(board);
+
   return (
     <div className={styles.container}>
       <h1>America Gon</h1>
       <InputTable size={size} board={board} setBoard={setBoard} />
       <ResetButton onReset={handleReset} />
+      <p>English word here</p>
     </div>
   );
 }
