a60665da4f6a0dbb243e7d921e0876c1a6a4a1db
SI <>
2024-06-01 10:10:44 +0900

    fix: refactoring getVerticalTopBottomPosition

--- src/hooks/searchDic.tsx
+++ src/hooks/searchDic.tsx
@@ -1,37 +1,22 @@
 type SearchDicResult = {
-  word: string;
-  matches: string[];
+  word: string;       // 検索キーワード（暫定で縦文字列のみ返している）
+  matches: string[];  // 辞書で見つかった文字列（暫定で縦文字列のみ返している）
 }
 
-const searchDic = async (board: string[][], row: number, col: number): Promise<SearchDicResult> => {
-    const response = await fetch('/kantan-ej-dictionary.json');
-  const data = await response.json();
+// 縦列の位置
+type VerticalPositionsResult = {
+  top: number;      // 縦の一番高い位置情報
+  bottom: number;   // 縦の一番低い位置情報
+}
 
-  // // 辞書データからクエリに一致する単語を検索
-  // if (data[query]) {
-  //   return data[query].ja;
-  // }
-  // return [];
-
-  console.log('searchDic', board, row, col);
-
-  // 上から下へ単語を構成
-  // 現在の位置から1マス上に戻って、上からチェックする必要がある。
-  // 上に言葉があれば、さらにその上に行く。言葉がなくなるまでこれを繰り返す。
-  // 言葉がなくなったら、その位置が最上部となる。この位置から現在位置までに成立する単語がないかを探す
-  // boardに盤面全体のコードが入っている。あとは現在位置が取得できればOK!
-  // let verticalWord = '';
-  // for (let i = 0; i < board.length; i++) {
-  //   verticalWord += board[i][col];
-
-  // }
-
-  // 上から下まで、文字がある端の位置を格納する。関数化したほうが良い。start *******************
-
-  // まず現在位置を取得
-  const currentCol = col; // 列
-  const currentRow = row; // 行
-
+/**
+ * 縦列の上下端の位置を取得する
+ * @param {string[][]} board 盤面の配列
+ * @param {number} row 現在の行の位置
+ * @param {number} col 現在の列の位置
+ * @returns {Promise<VerticalPositionsResult>} 縦列の端の位置情報
+*/
+const getVerticalTopBottomPosition = (board: string[][], row:number, col:number):Promise<VerticalPositionsResult> =>{
   // 上下の一番端を記憶するためのローカル変数
   let verticalHighPosition = row;
 
@@ -45,14 +30,8 @@ const searchDic = async (board: string[][], row: number, col: number): Promise<S
     verticalHighPosition = i;
   }
 
-  console.log('verticalHighPosition=', verticalHighPosition);
-
-
   let verticalLowPosition = row;
 
-  console.log('board[row].length=', board[row].length);
-  console.log('board[col].length=', board[col].length);
-  console.log('board.length=', board.length);
   // 現在位置から１マス下をチェックする
   for(let i=row; i < board[row].length; i++){
     // もし値が入っていなかったら、その時点でループを抜ける
@@ -63,28 +42,40 @@ const searchDic = async (board: string[][], row: number, col: number): Promise<S
     verticalLowPosition = i;
   }
 
-  console.log('verticalHighPosition=', verticalHighPosition);
-  console.log('verticalLowPosition=', verticalLowPosition);
+  const result: VerticalPositionsResult = {
+    top:verticalHighPosition,
+    bottom:verticalLowPosition,
+  }
+  return Promise.resolve(result);
+}
 
 
-  // 上から下まで、文字がある端の位置を格納する。関数化したほうが良い。end *******************
+/**
+ * 辞書検索を行う
+ * @param {string[][]} board 盤面の配列
+ * @param {number} row 行の番号
+ * @param {number} col 列の番号
+ * @returns {Promise<SearchDicResult>} 検索結果
+ */
+const searchDic = async (board: string[][], row: number, col: number): Promise<SearchDicResult> => {
+  const response = await fetch('/kantan-ej-dictionary.json');
+  const data = await response.json();
 
+  const resultVerticalTopBottomPosition = await getVerticalTopBottomPosition(board, row, col);
+  console.log(resultVerticalTopBottomPosition.top);
+  console.log(resultVerticalTopBottomPosition.bottom);
 
-  // 文字列を連結。関数化を検討したほうが良いかも。
-  // (i < rowでいったん現在位置までを確認。本来は一番下まで確認しないといけない)
+  // 文字列を連結。関数化を検討したほうが良いかも。start******************
 
   let verticalWord = '';
 
-  for(let i =verticalHighPosition; i <= verticalLowPosition; i++){
+  for(let i =resultVerticalTopBottomPosition.top; i <= resultVerticalTopBottomPosition.bottom; i++){
     verticalWord += board[i][col];
   }
 
-  console.log('created verticalWord=', verticalWord);
+  // 文字列を連結。関数化を検討したほうが良いかも。end ******************
 
-  // 現在位置から上だけでなく、下もチェックしないといけないね。
-  // 文字が入っている一番上の位置と、一番下の位置を覚えて、端から端までを連結する必要がある。
-
-  // 左から右へ単語を構成する場合も、上下と同様に対応する。こりゃ大変だぁ。
+  // TODO: 左から右へ単語を構成する場合も、上下と同様に対応する。こりゃ大変だぁ。
 
   // 単語が辞書に存在するかチェック
   const verticalMatches = data[verticalWord] ? data[verticalWord].ja : [];
